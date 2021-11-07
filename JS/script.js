5fb64d793dd4dc89789fbc4b35ea27bcb4a53483
AhmedShehata98 <userstore98@gmail.com>
2021-11-07 14:52:06 +0200

    v2.5

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,13 @@ const search_bar = document.querySelector('#search_bar')
 const search_ico = document.querySelector('.search_ico')
 const bar_contain= document.querySelector('.container')
 search_bar.addEventListener('focus',()=>{
-    search_ico.style.display="none"
-    console.log("oops")
+    search_ico.style.opacity="0"
 })
-
-bar_contain.addEventListener("click",()=>{
-    search_ico.style.display="flex"
-    console.log("okay");
+search_bar.addEventListener('focusout',()=>{
+    search_ico.style.opacity="1"
 })
+// bar_contain.addEventListener("click",()=>{
+//     search_ico.style.display="flex"
+//     console.log("okay");
+// })
 console.log(search_ico);
